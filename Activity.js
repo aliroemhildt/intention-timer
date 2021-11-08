b9987acb9c3401f64fea18308cd4d8bb2ef222ec
Rana Jurjus <rjurjus11@mail.bw.edu>
2021-11-07 22:35:01 -0500

    Refactor global variables

--- Activity.js
+++ Activity.js
@@ -5,12 +5,27 @@ class Activity {
     this.minutes = minutes;
     this.seconds = seconds;
     this.completed = false;
-    this.id = 'activity'+Date.now();
+    this.id = "activity" + Date.now();
   };
 
-  startTimer() {
-    startTime = Date.now();
-    updateTimer();
+  startTimer(renderRemainingTime, onDone) {
+    this.startTime = Date.now();
+    function everyInterval() {
+      var currentTime = Date.now();
+      var elapsedTime = currentTime - this.startTime;
+      var overAllDuration = (this.minutes * 60 + this.seconds) * 1000;
+      var remainingDuration = overAllDuration - elapsedTime;
+      if (remainingDuration <= 0) {
+        onDone();
+        clearInterval(this.intervalID);
+        return;
+      };
+      var remainingSeconds = Math.floor(remainingDuration / 1000);
+      var minutesComponent = Math.floor(remainingSeconds / 60);
+      var secondsComponent = Math.floor(remainingSeconds % 60);
+      renderRemainingTime(minutesComponent, secondsComponent);
+    };
+    this.intervalID = setInterval(everyInterval.bind(this), 100);
   };
 
   markComplete() {
